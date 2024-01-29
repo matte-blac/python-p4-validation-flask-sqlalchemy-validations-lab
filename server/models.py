2fdde5f85604bf681d09968583d2e7f5f257b0fd
Matthew Bomett <bomettmat2@gmail.com>
2024-01-29 11:53:33 +0300

    Done

--- server/models.py
+++ server/models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+import re
+
 db = SQLAlchemy()
 
 class Author(db.Model):
@@ -12,6 +14,15 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        assert name is not None, 'Author must have a name'
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        assert phone_number is not None and len(phone_number) == 10, 'Phone number must be exactly 10 digits.'
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,6 +39,39 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates('title')
+    def validate_title(self, key, title):
+        assert title is not None, 'Post must have a title.'
+        assert self.is_clickbait(title), 'Title must be clickbait'
+        return title
+    
+    @staticmethod
+    def is_clickbait(title):
+        patterns = [
+            "Won't Believe",
+            "Secret",
+            r"Top \d+",
+            "Guess"
+        ]
+        for pattern in patterns:
+            if re.search(pattern, title, re.IGNORECASE):
+                return True
+        return False
+    
+    @validates('content')
+    def validate_content(self, key, content):
+        assert content is not None and len(content) >= 250, 'Post content must be at least 250 characters.'
+        return content
+    
+    @validates('summary')
+    def validates_summary(self, key, summary):
+        assert summary is not None and len(summary) < 250, ' Post summary must be a maximum of 250 characters.'
+        return summary
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        assert category in ['Fiction', 'Non-Fiction'], "Post category must be either 'Fiction' or 'Non-Fiction'."
+        return category
 
 
     def __repr__(self):
